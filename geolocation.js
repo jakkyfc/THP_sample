c1a7b783e3b1ebe2b2d24b5275009db83416a6ea
jakkyfc <jakky.lin@gmail.com>
2022-10-20 22:47:12 +0900

    add geolocation api

---
+++ geolocation.js
@@ -0,0 +1,31 @@
+/**
+ * https://developer.mozilla.org/ja/docs/Web/API/Geolocation/watchPosition#%E5%BC%95%E6%95%B0
+ */
+
+let id, target, options;
+
+function success(pos) {
+  let crd = pos.coords;
+
+  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+    console.log('Congratulations, you reached the target');
+    navigator.geolocation.clearWatch(id);
+  }
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+target = {
+  latitude : 0,
+  longitude: 0
+};
+
+options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
