6efe622b14b0b591c421b19a743eec1f29a9a439
jakkyfc <jakky.lin@gmail.com>
2022-10-21 11:33:03 +0900

    緯度経度を画面上に表示

--- geolocation.js
+++ geolocation.js
@@ -2,11 +2,14 @@
  * https://developer.mozilla.org/ja/docs/Web/API/Geolocation/watchPosition#%E5%BC%95%E6%95%B0
  */
 
-let id, target, options;
+let id, target, options, log;
+let textArea = document.getElementById("geo");
 
 function success(pos) {
   let crd = pos.coords;
-  console.log(`Your current position is: ${crd.latitude} , ${crd.longitude}. (${crd.accuracy} )`);
+  log = (`${Date.now()},${crd.latitude},${crd.longitude},${crd.accuracy}\r\n`);
+  textArea.value += log;
+  console.log(log);
 }
 
 function error(err) {
